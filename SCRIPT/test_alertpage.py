fa3063c686466cc3a4051d8da5dba6d81dca7393
Miao Sang <118147601+Sarahsang@users.noreply.github.com>
2023-10-19 01:41:54 +1300

    Update test_alertpage.py

---
+++ SCRIPT/test_alertpage.py
@@ -0,0 +1,33 @@
+import pytest
+from Base.base import BaseSetup
+from PO.Home_Page import HomePage
+from PO.AlertPage import AlertPage
+
+class TestAlertPage:
+
+    @pytest.fixture(scope="function")
+    def setup(self):
+        self.driver = BaseSetup.initialize_driver()
+        self.driver.implicitly_wait(10)
+        yield
+        self.driver.quit()
+
+    def test_alerts(self, setup):
+        # Initialize HomePage and AlertPage objects
+        home_page = HomePage(self.driver)
+        alert_page = AlertPage(self.driver)
+
+        # Navigate to the home page and click the alert button
+        home_page.click_dialog_button()  # Assuming you have a method named click_alert_button in HomePage class
+
+        # 1. Accept the simple alert
+        alert_page.handle_simple_alert()
+
+        # 2. Dismiss the confirm alert and print the alert text
+        alert_page.handle_confirm_alert()
+
+        # 3. Type your name and accept the prompt alert
+        alert_page.handle_prompt_alert("Your Name")
+
+        # 4. Handle the modern alert
+        alert_page.handle_modern_alert()
