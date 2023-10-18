b519f8d3ebf738ad0f1c9fac1124eec934da64bb
Miao Sang <118147601+Sarahsang@users.noreply.github.com>
2023-10-18 21:59:51 +1300

    update

--- PO/AlertPage.py
+++ PO/AlertPage.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.alert import Alert
 
 class AlertPage:
 
@@ -11,4 +12,41 @@ class AlertPage:
         self.third_select_box = (By.XPATH, "//button[@id='prompt']")
         self.fourth_select_box = (By.XPATH, "//button[@id='modern']")
 
-    
+    def handle_simple_alert(self):
+        self.driver.find_element(*self.first_alert_button).click()
+        alert = Alert(self.driver)
+        alert.accept()
+
+    def handle_confirm_alert(self):
+        self.driver.find_element(*self.second_alert_button).click()
+        alert = Alert(self.driver)
+        print(alert.text)
+        alert.dismiss()
+
+    def handle_prompt_alert(self, name):
+        self.driver.find_element(*self.third_alert_button).click()
+        alert = Alert(self.driver)
+        alert.send_keys(name)
+        alert.accept()
+
+    def handle_modern_alert(self):
+        self.driver.find_element(*self.fourth_alert_button).click()
+        
+    def handle_simple_alert(self):
+        self.driver.find_element(*self.first_alert_button).click()
+        self.driver.switch_to.alert.accept()
+
+    def handle_confirm_alert(self):
+        self.driver.find_element(*self.second_alert_button).click()
+        alert = self.driver.switch_to.alert
+        print(alert.text)
+        alert.dismiss()
+
+    def handle_prompt_alert(self, name):
+        self.driver.find_element(*self.third_alert_button).click()
+        alert = self.driver.switch_to.alert
+        alert.send_keys(name)
+        alert.accept()
+
+    def handle_modern_alert(self):
+        self.driver.find_element(*self.fourth_alert_button).click()
